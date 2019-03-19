c17738be7beb69b112bdfc4709b3f27bd981dd90
Timur Badretdinov <destinerx@gmail.com>
2019-03-19 17:40:44 +0300

    Add checkAllowance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import { ethers } from 'ethers';
 
+import erc20Abi from '../abi/erc20.json';
+
 export class DexAgSdk {
 	constructor(provider) {
 		this.provider = new ethers.providers.Web3Provider(provider);
 		this.signer = this.provider.getSigner();
+	}
+
+	async checkAllowance(tradeMetadata) {
+		if (!tradeMetadata.token) {
+			return;
+		}
+		const tokenAddress = tradeMetadata.token.address;
+		const exchangeAddress = tradeMetadata.token.spender;
+		const tokenContract = new ethers.Contract(tokenAddress, erc20Abi, this.provider);
+		const address = await this.signer.getAddress();
+		const allowance = await tokenContract.allowance(address, exchangeAddress)
+		const tokenAmount = tradeMetadata.token.amount;
+		return allowance.gte(tokenAmount);
 	}
 
 	async sendTrade(trade) {
